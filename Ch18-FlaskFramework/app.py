993b1986be05b0c88b50711a8d21b7b0e11c953a
developersview <pcslg1998@gmail.com>
2025-05-28 10:31:58 +0530

    Ch18 - Flask in Python

--- Ch18-FlaskFramework/app.py
+++ Ch18-FlaskFramework/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 '''
  It creates an instance of the Flask class, 
@@ -11,13 +11,22 @@ app = Flask(__name__)
 def welcome():
     return "Hello Team. This is the first Flask application."
 
-@app.route("/index")
+@app.route("/index", methods=['GET'])
 def index():
     return render_template("index.html")
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/form", methods=['GET', 'POST'])
+def route():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f"Hello {name}, welcome to the Flask application!"
+    else:
+        return render_template("form.html")
+
 
 if __name__ == '__main__':
     try:
